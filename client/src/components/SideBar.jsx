aabf22b58eee5c646b300a1a14afe65be9acddd6
Shane Yuan <shaneyuan18@gmail.com>
2018-11-05 15:42:58 -0800

    set up sidebar css

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import styles from './styles/Sidebar.css';
 
 const Sidebar = (props) => (
-  <div>
+  <div className={styles.sidebar}>
     <a className="active" href="#home">Home</a>
     <a href="#profolio">Profolio</a>
     <a href="#contact">Contact</a>
