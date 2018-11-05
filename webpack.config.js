f64af5a1865e4d0a9f1ce17b55051763f03565dd
Shane Yuan <shaneyuan18@gmail.com>
2018-11-04 19:13:56 -0800

    webpack set up

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/react', '@babel/env'],
+        },
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]' 
+      },
+    ],
+  },
+};
